588b9ff2d5d6a9c125caf6ca6a01bf85949cfa71
Xi Chen <407081305@qq.com>
2025-01-13 13:21:21 +0800

    install gymnasium

---
+++ examples/00_basic_usage.py
@@ -0,0 +1,13 @@
+#Ref: https://gymnasium.farama.org/introduction/basic_usage/
+
+import gymnasium as gym
+
+env = gym.make('LunarLander-v3', render_mode='human')
+observation, info = env.reset()
+
+episode_over = False
+while not episode_over:
+    action = env.action_space.sample() # agent policy that uses the observation and info
+    next_observation, reward, terminated, truncated, info = env.step(action)
+    episode_over = terminated or truncated
+env.close()
---
+++ pip_installation_node.md
@@ -0,0 +1,10 @@
+## install gymnasium
+Ref: https://stackoverflow.com/questions/76222239/pip-install-gymnasiumbox2d-not-working-on-google-colab
+```
+pip install wheel setuptools pip --upgrade
+pip install swig
+```
+
+```
+pip install gymnasium[box2d]
+```
