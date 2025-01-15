6fd89be48be2486bf49f9c079cc6a43c75adaada
Xi Chen <407081305@qq.com>
2025-01-15 14:02:42 +0800

    runable but buggy ver for deep qlearning to solve backjack

--- examples/02_dql_blackjack.py
+++ examples/02_dql_blackjack.py
@@ -6,10 +6,13 @@ import gymnasium as gym
 import numpy as np
 from collections import defaultdict
 
+import torch
 import torch.nn as nn
-import numpy as np
+import torch.nn.functional as F
 
-n_episode = 100_000
+from stable_baselines3.common.buffers import ReplayBuffer
+
+n_episode = 10_000
 Render_ON = False
 if Render_ON:
     env = gym.make('Blackjack-v1', sab=False, render_mode='human')
@@ -23,25 +26,56 @@ class ValueNetwork(nn.Module):
     def __init__(self, env):
         super().__init__()
         self.network = nn.Sequential(
-            nn.Linear(np.array(env.single_observation_space.shape).prod(), 120),
-            nn.Relu(),
+            nn.Linear(len(env.observation_space), 120),
+            nn.ReLU(),
             nn.Linear(120, 84),
-            nn.Relu(),
-            nn.Linear(84, env.single_observation_space.n)
+            nn.ReLU(),
+            nn.Linear(84, env.action_space.n)
         )
     def forward(self, x):
         return self.network(x)
 
-    
+# Replay buffer
+# class ReplayBuffer:
+#     def __init__(self, max_size):
+#         self.buffer = []
+#         self.max_size = max_size
+
+#     def add(self, experience):
+#         if len(self.buffer) >= self.max_size:
+#             self.buffer.pop(0)
+#         self.buffer.append(experience)
+
+#     def sample(self, batch_size):
+#         indices = np.random.choice(len(self.buffer), batch_size, replace=False)
+#         return [self.buffer[i] for i in indices]
+       
 # create the agent
 class BlackjackAgent:
-    def __init__(self, env):
+    def __init__(self, env, device, buffer_size=512, batch_size=32):
         self.env = env
         self.q_table = defaultdict(lambda: np.zeros(env.action_space.n))
         self.epsilon = 1.0
         self.discount_factor = 0.95
         self.lr = 0.01
         self.training_error = []
+        self.device = device
+        self.q_network = ValueNetwork(env).to(device)
+        self.target_network = ValueNetwork(env).to(device)
+        self.optimizer = torch.optim.Adam(self.q_network.parameters(), lr=self.lr)
+        self.batch_size = batch_size
+        
+        self.rb = ReplayBuffer(
+            buffer_size=buffer_size,
+            observation_space=gym.spaces.MultiDiscrete([21, 10, 1]), # Example: x in [0,21], y in [0,10] z in [0,1]
+            action_space=gym.spaces.Discrete(env.action_space.n), # gym.spaces.Discrete(2), Example action space with two possible actions
+            device=device,
+            handle_timeout_termination=False,
+        )
+    
+    # def _linear_schedule(start_e: float, end_e: float, duration: int, t: int) -> float:
+    #     slope = (end_e - start_e) / duration
+    #     return max(end_e, start_e + slope * t)
     
     def get_action(self, obs):
         # with probability epsilon return a random action to explore the environment
@@ -49,26 +83,53 @@ class BlackjackAgent:
             return self.env.action_space.sample()
         # with the probability (1 - epsilon) act greedily (exploit)
         else:
-            return int(np.argmax(self.q_table[obs]))
+            q_values = self.q_network(torch.Tensor(obs).to(self.device))
+            actions = torch.argmax(q_values, dim=0).cpu().numpy()
+            return actions
+        # epsilon = self._linear_schedule(1.0, 0.1, 1_000_000, t)
+        
     
     def update(self, 
                obs,
                action, 
                reward, 
                next_obs,
-               terminated):
-        """Update the Q value network of an action"""
-        future_q_value = (not terminated) * np.max(self.q_table[next_obs])
-        temporal_difference = reward + self.discount_factor * future_q_value - self.q_table[obs][action]
+               terminated,
+               info):
+        # """Update the Q value network of an action"""
+        # future_q_value = (not terminated) * np.max(self.q_table[next_obs])
+        # temporal_difference = reward + self.discount_factor * future_q_value - self.q_table[obs][action]
         
-        self.q_table[obs][action] += self.lr * temporal_difference
-        self.training_error.append(temporal_difference)
+        # self.q_table[obs][action] += self.lr * temporal_difference
+        # self.training_error.append(temporal_difference)
+        # self.rb.add((obs, next_obs, action, reward, terminated, info))
+        self.rb.add(np.array(obs), np.array(next_obs), action, reward, terminated, info)
+        if self.rb.size() > self.batch_size:
+            data = self.rb.sample(self.batch_size)
+            # obs, next_obs, action, reward, terminated, info = zip(*data)
+            # next_obs = torch.Tensor(np.vstack(next_obs)).to(self.device)
+            # terminated = torch.Tensor(np.vstack(terminated)).to(self.device)
+            # reward= torch.Tensor(np.vstack(reward)).to(self.device)
+            # obs = torch.Tensor(np.vstack(obs)).to(self.device)
+            with torch.no_grad():
+                target_max, _ = self.target_network(data.next_observations.to(torch.float)).max(dim=1)
+                td_target = data.rewards.to(torch.float) + self.discount_factor * target_max * (1 - data.dones.to(torch.float))
+            old_values = self.q_network(data.observations.to(torch.float)).gather(1, data.actions)
+            loss = F.mse_loss(td_target, old_values)
+            self.training_error.append(loss.item())
+            
+            # optimize the model
+            self.optimizer.zero_grad()
+            loss.backward()
+            self.optimizer.step()
     
     def decay_epsilon(self):
         self.epsilon = max(0.1, self.epsilon - 1e-5)
 
 # hyperparameters
-agent = BlackjackAgent(env)
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+agent = BlackjackAgent(env,
+                       device=device,)
 
 # loop over episodes
 from tqdm import tqdm
@@ -83,7 +144,7 @@ for episode in tqdm(range(n_episode)):
         next_obs, reward, terminated, trucated, info = env.step(action)
         
         # update the agent
-        agent.update(obs, action, reward, next_obs, terminated)
+        agent.update(obs, action, reward, next_obs, terminated, info)
         
         # update if the environment is done and the current obs
         done = terminated or trucated
