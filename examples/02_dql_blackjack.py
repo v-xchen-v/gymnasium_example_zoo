358f05e9485c1140a06450339036e60c0f8be3e2
Xi Chen <407081305@qq.com>
2025-01-15 14:38:02 +0800

    A maybe worked ver

--- examples/02_dql_blackjack.py
+++ examples/02_dql_blackjack.py
@@ -12,7 +12,7 @@ import torch.nn.functional as F
 
 from stable_baselines3.common.buffers import ReplayBuffer
 
-n_episode = 10_000
+n_episode = 100_000
 Render_ON = False
 if Render_ON:
     env = gym.make('Blackjack-v1', sab=False, render_mode='human')
@@ -35,26 +35,10 @@ class ValueNetwork(nn.Module):
     def forward(self, x):
         return self.network(x)
 
-# Replay buffer
-# class ReplayBuffer:
-#     def __init__(self, max_size):
-#         self.buffer = []
-#         self.max_size = max_size
-
-#     def add(self, experience):
-#         if len(self.buffer) >= self.max_size:
-#             self.buffer.pop(0)
-#         self.buffer.append(experience)
-
-#     def sample(self, batch_size):
-#         indices = np.random.choice(len(self.buffer), batch_size, replace=False)
-#         return [self.buffer[i] for i in indices]
-       
 # create the agent
 class BlackjackAgent:
     def __init__(self, env, device, buffer_size=512, batch_size=32):
         self.env = env
-        self.q_table = defaultdict(lambda: np.zeros(env.action_space.n))
         self.epsilon = 1.0
         self.discount_factor = 0.95
         self.lr = 0.01
@@ -62,6 +46,7 @@ class BlackjackAgent:
         self.device = device
         self.q_network = ValueNetwork(env).to(device)
         self.target_network = ValueNetwork(env).to(device)
+        self.target_network.load_state_dict(self.q_network.state_dict())
         self.optimizer = torch.optim.Adam(self.q_network.parameters(), lr=self.lr)
         self.batch_size = batch_size
         
@@ -73,9 +58,6 @@ class BlackjackAgent:
             handle_timeout_termination=False,
         )
     
-    # def _linear_schedule(start_e: float, end_e: float, duration: int, t: int) -> float:
-    #     slope = (end_e - start_e) / duration
-    #     return max(end_e, start_e + slope * t)
     
     def get_action(self, obs):
         # with probability epsilon return a random action to explore the environment
@@ -85,9 +67,7 @@ class BlackjackAgent:
         else:
             q_values = self.q_network(torch.Tensor(obs).to(self.device))
             actions = torch.argmax(q_values, dim=0).cpu().numpy()
-            return actions
-        # epsilon = self._linear_schedule(1.0, 0.1, 1_000_000, t)
-        
+            return actions        
     
     def update(self, 
                obs,
@@ -96,25 +76,14 @@ class BlackjackAgent:
                next_obs,
                terminated,
                info):
-        # """Update the Q value network of an action"""
-        # future_q_value = (not terminated) * np.max(self.q_table[next_obs])
-        # temporal_difference = reward + self.discount_factor * future_q_value - self.q_table[obs][action]
-        
-        # self.q_table[obs][action] += self.lr * temporal_difference
-        # self.training_error.append(temporal_difference)
-        # self.rb.add((obs, next_obs, action, reward, terminated, info))
+        """Update the Q network with the new observation."""
         self.rb.add(np.array(obs), np.array(next_obs), action, reward, terminated, info)
         if self.rb.size() > self.batch_size:
             data = self.rb.sample(self.batch_size)
-            # obs, next_obs, action, reward, terminated, info = zip(*data)
-            # next_obs = torch.Tensor(np.vstack(next_obs)).to(self.device)
-            # terminated = torch.Tensor(np.vstack(terminated)).to(self.device)
-            # reward= torch.Tensor(np.vstack(reward)).to(self.device)
-            # obs = torch.Tensor(np.vstack(obs)).to(self.device)
             with torch.no_grad():
                 target_max, _ = self.target_network(data.next_observations.to(torch.float)).max(dim=1)
-                td_target = data.rewards.to(torch.float) + self.discount_factor * target_max * (1 - data.dones.to(torch.float))
-            old_values = self.q_network(data.observations.to(torch.float)).gather(1, data.actions)
+                td_target = data.rewards.flatten().to(torch.float) + self.discount_factor * target_max * (1 - data.dones.flatten().to(torch.float))
+            old_values = self.q_network(data.observations.to(torch.float)).gather(1, data.actions).squeeze()
             loss = F.mse_loss(td_target, old_values)
             self.training_error.append(loss.item())
             
@@ -122,6 +91,12 @@ class BlackjackAgent:
             self.optimizer.zero_grad()
             loss.backward()
             self.optimizer.step()
+            
+            tau=1.0
+            for target_network_param, q_network_param in zip(self.target_network.parameters(), self.q_network.parameters()):
+                target_network_param.data.copy_(
+                    tau * q_network_param.data + (1.0 - tau) * target_network_param.data
+                )
     
     def decay_epsilon(self):
         self.epsilon = max(0.1, self.epsilon - 1e-5)
